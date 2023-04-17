6cf76da672ba883f88ea4b5f063eb847c14b35e8
Данил Волконеско <eclipsemod555@gmail.com>
2023-04-17 19:54:31 +0300

    reviews refactor

--- src/components/ReviewsBlock/index.tsx
+++ src/components/ReviewsBlock/index.tsx
@@ -34,45 +34,45 @@ const ReviewsBlock = () => {
 										</Box>
 										<Box className={styles.box}>
 												<Stack justifyContent='center' textAlign='center' padding='50px 10%' spacing={4}>
-														<p className={styles.text}>Хорошие ребята! Приехали в обозначенное время, заранее позвонили, хорошо почистили матрас, были очень приветливы. По деньгам взяли ровно столько, сколько обговаривали по телефону. В общем, впечатления самые хорошие!</p>
+														<p className={styles.text}>Отдичный сервис,быстро и качественно, пользуюсь услугой химчистки второй, полностью доволен результатам</p>
 														<Stack spacing={1}>
-																<p className={styles.name}>Наталья С.</p>
+																<p className={styles.name}>Михаил</p>
 																<span className={styles.city}>Армавир</span>
 														</Stack>
 												</Stack>
 										</Box>
 										<Box className={styles.box}>
 												<Stack justifyContent='center' textAlign='center' padding='50px 10%' spacing={4}>
-														<p className={styles.text}>Хорошие ребята! Приехали в обозначенное время, заранее позвонили, хорошо почистили матрас, были очень приветливы. По деньгам взяли ровно столько, сколько обговаривали по телефону. В общем, впечатления самые хорошие!</p>
+														<p className={styles.text}>Спасибо большое за быструю качественную работу. Сделали химчистку большого Дивана после переезда. Диван высох за ночь. Не осталось ни запахов ни пятен. Быстро четко приняли заявку, отработали и дали скидку. Рекомендуем.</p>
 														<Stack spacing={1}>
-																<p className={styles.name}>Наталья С.</p>
+																<p className={styles.name}>Валерий М.</p>
 																<span className={styles.city}>Армавир</span>
 														</Stack>
 												</Stack>
 										</Box>
 										<Box className={styles.box}>
 												<Stack justifyContent='center' textAlign='center' padding='50px 10%' spacing={4}>
-														<p className={styles.text}>Хорошие ребята! Приехали в обозначенное время, заранее позвонили, хорошо почистили матрас, были очень приветливы. По деньгам взяли ровно столько, сколько обговаривали по телефону. В общем, впечатления самые хорошие!</p>
+														<p className={styles.text}>Дважды пользовалась услугами химчиски, светлый диван был как новый. Вчера приходили чистить матрас. Есть удобная функция напоминания раз в год. Супер быстро, надежно и чисто. Спасибо большое Вам и Вашим мастерам.</p>
 														<Stack spacing={1}>
-																<p className={styles.name}>Наталья С.</p>
+																<p className={styles.name}>Оксана В.</p>
 																<span className={styles.city}>Армавир</span>
 														</Stack>
 												</Stack>
 										</Box>
 										<Box className={styles.box}>
 												<Stack justifyContent='center' textAlign='center' padding='50px 10%' spacing={4}>
-														<p className={styles.text}>Хорошие ребята! Приехали в обозначенное время, заранее позвонили, хорошо почистили матрас, были очень приветливы. По деньгам взяли ровно столько, сколько обговаривали по телефону. В общем, впечатления самые хорошие!</p>
+														<p className={styles.text}>Вызываю уже не первый раз. Как всегда всё на высоте. Дмитрий - очень вежливый и аккуратный сотрудник. Советую эту компанию, не пожалеете!</p>
 														<Stack spacing={1}>
-																<p className={styles.name}>Наталья С.</p>
+																<p className={styles.name}>Алексей</p>
 																<span className={styles.city}>Армавир</span>
 														</Stack>
 												</Stack>
 										</Box>
 										<Box className={styles.box}>
 												<Stack justifyContent='center' textAlign='center' padding='50px 10%' spacing={4}>
-														<p className={styles.text}>Хорошие ребята! Приехали в обозначенное время, заранее позвонили, хорошо почистили матрас, были очень приветливы. По деньгам взяли ровно столько, сколько обговаривали по телефону. В общем, впечатления самые хорошие!</p>
+														<p className={styles.text}>Очень качествено, быстро. Большое спасибо.</p>
 														<Stack spacing={1}>
-																<p className={styles.name}>Наталья С.</p>
+																<p className={styles.name}>Людмила Свиридович</p>
 																<span className={styles.city}>Армавир</span>
 														</Stack>
 												</Stack>
