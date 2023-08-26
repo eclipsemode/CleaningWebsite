41b97e20053024cd532cd5be6871be16dc98f9d1
Данил Волконеско <eclipsemod555@gmail.com>
2023-08-26 16:30:07 +0300

    accordion fix

--- src/components/QuestionAndAnswerBlock/index.tsx
+++ src/components/QuestionAndAnswerBlock/index.tsx
@@ -1,12 +1,73 @@
 import React from 'react';
 import Title from "../../ui/Title";
-import {AccordionDetails, AccordionSummary, Box, Container, Stack, Typography} from "@lib/mui";
-import {ExpandMoreIcon} from '@lib/mui-icons';
+import {Box, Container, Stack} from "@lib/mui";
 import {Element} from '@lib/react-scroll'
-import AccordionCustom from "@components/QuestionAndAnswerBlock/ui/AccordionCustom";
+import AccordionCustom, {IAccordion} from "@components/QuestionAndAnswerBlock/ui/AccordionCustom";
 
 const QuestionAndAnswerBlock = () => {
-    const renderExpandIcon = () => <ExpandMoreIcon sx={{color: '#0360c0', width: '40px', height: '40px'}}/>
+
+    const accordions: IAccordion[] = [
+        {
+            name: 'panel1',
+            title: 'Неприятный запах от животного можете удалить?',
+            description: 'Да. Нейтрализуем запах с гарантией результата. Если запах вновь проявится, повторная нейтрализация БЕСПЛАТНО.'
+        },
+        {
+            name: 'panel2',
+            title: 'Нужно уходить из дома во время чистки?',
+            description: 'Нет, процедура полностью безопасна, мы не используем средства с резкими запахами. Можно находиться дома и даже смотреть за работой.'
+        },
+        {
+            name: 'panel3',
+            title: 'Неприятный запах от животного можете удалить?',
+            description: 'Чистка подразумевает удаление всех загрязнений с обивки, в том числе и шерсть животных.'
+        },
+        {
+            name: 'panel4',
+            title: 'У Меня аллергия на моющие средства, могу ли я воспользоваться услугой?',
+            description: 'Да, можете. Технология чистки подразумевает полное удаление остатков моющих средств из обивки. Обязательно предупредите мастера про аллергию и он сделает дополнительную промывку чистой водой.'
+        },
+        {
+            name: 'panel5',
+            title: 'Когда сможете приехать?',
+            description: 'Работаем без выходных, выезжаем в любое удобное время дня и ночи.'
+        },
+        {
+            name: 'panel6',
+            title: 'От чего зависит стоимость чистки?',
+            description: 'Стоимость зависит от: Размера изделия, Формы спинок и подлокотников, Степени загрязнения/наличия запаха, Материала обивки, Наличия съемных подушек'
+        },
+        {
+            name: 'panel7',
+            title: 'С организациями работаете?',
+            description: 'Работаем с любыми организациями по договору, через расчетный счет.'
+        },
+        {
+            name: 'panel8',
+            title: 'Сколько сохнет после чистки?',
+            description: 'Экспресс сушка - 2-3 часа, самостоятельное высыхание 6-10 часов'
+        },
+        {
+            name: 'panel9',
+            title: 'Сколько времени занимает чистка?',
+            description: 'Среднее время чистки одного изделия 2-4 часа'
+        },
+        {
+            name: 'panel10',
+            title: 'В область выезжаете?',
+            description: 'Да, выезжаем. В ближайшие города области выезд бесплатный, выезд в отдаленные областные города и районы обговаривается индивидуально.'
+        },
+        {
+            name: 'panel11',
+            title: 'Процедура безопасна для детей и животных?',
+            description: 'Технология полностью безопасна для детей и животных. Процедура проходит без резких неприятных запахов, можно находиться дома и наблюдать за процессом.'
+        },
+        {
+            name: 'panel12',
+            title: 'Какие способы оплаты?',
+            description: 'Наличные, перевод на карту, оплата на расчетный счет.'
+        }
+    ]
 
     return (
         <section>
@@ -16,231 +77,7 @@ const QuestionAndAnswerBlock = () => {
                         <Title color='secondary' textMain='вопросы' textSecondary='и ответы'/>
                     </Element>
                     <Box>
-                        <AccordionCustom name='panel1'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel1bh-content"
-                                    id="panel1bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>Неприятный запах от животного можете
-                                        удалить?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Да. Нейтрализуем запах с гарантией результата. Если запах вновь проявится,
-                                        повторная
-                                        нейтрализация БЕСПЛАТНО.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-                        <AccordionCustom name='panel2'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel2bh-content"
-                                    id="panel2bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>
-                                        Нужно уходить из дома во время чистки?
-                                    </Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Нет, процедура полностью безопасна, мы не используем средства с резкими
-                                        запахами.
-                                        Можно находиться дома и даже смотреть за работой.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-                        <AccordionCustom name='panel3'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel3bh-content"
-                                    id="panel3bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>
-                                        Шерсть животных можете убрать?
-                                    </Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Чистка подразумевает удаление всех загрязнений с обивки, в том числе и шерсть
-                                        животных.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel4'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>У Меня аллергия на моющие средства, могу ли я
-                                        воспользоваться услугой?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Да, можете. Технология чистки подразумевает полное удаление остатков моющих
-                                        средств
-                                        из обивки. Обязательно предупредите мастера про аллергию и он сделает
-                                        дополнительную
-                                        промывку чистой водой.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel5'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>Когда сможете приехать?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Работаем без выходных, выезжаем в любое удобное время дня и ночи.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel6'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>От чего зависит стоимость чистки?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Стоимость зависит от:<br/>
-                                        Размера изделия<br/>
-                                        Формы спинок и подлокотников<br/>
-                                        Степени загрязнения/наличия запаха<br/>
-                                        Материала обивки<br/>
-                                        Наличия съемных подушек
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel7'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>С организациями работаете?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Работаем с любыми организациями по договору, через расчетный счет.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel8'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>Сколько сохнет после чистки?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Экспресс сушка - 2-3 часа, самостоятельное высыхание 6-10 часов
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel9'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>Сколько времени занимает чистка?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Среднее время чистки одного изделия 2-4 часа
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel10'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>В область выезжаете?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Да, выезжаем. В ближайшие города области выезд бесплатный, выезд в отдаленные
-                                        областные города и районы обговаривается индивидуально.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel11'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>Процедура безопасна для детей и
-                                        животных?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Технология полностью безопасна для детей и животных. Процедура проходит без
-                                        резких
-                                        неприятных запахов, можно находиться дома и наблюдать за процессом.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
-
-                        <AccordionCustom name='panel12'>
-                            <div itemScope itemType="https://schema.org/Question">
-                                <AccordionSummary
-                                    expandIcon={renderExpandIcon()}
-                                    aria-controls="panel4bh-content"
-                                    id="panel4bh-header"
-                                >
-                                    <Typography itemProp="name" sx={{fontWeight: 600}}>Какие способы оплаты?</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails itemProp="acceptedAnswer" itemScope itemType="https://schema.org/Answer">
-                                    <Typography itemProp="text" sx={{color: 'text.secondary'}}>
-                                        Наличные, перевод на карту, оплата на расчетный счет.
-                                    </Typography>
-                                </AccordionDetails>
-                            </div>
-                        </AccordionCustom>
+                        <AccordionCustom accordions={accordions}/>
                     </Box>
                 </Stack>
             </Container>
--- src/components/QuestionAndAnswerBlock/ui/AccordionCustom.tsx
+++ src/components/QuestionAndAnswerBlock/ui/AccordionCustom.tsx
@@ -1,18 +1,46 @@
 "use client"
 import React from 'react';
 import {Accordion} from "@lib/mui";
+import {AccordionDetails, AccordionSummary, Typography} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-const AccordionCustom = ({children, name}: {children: React.JSX.Element, name: string}) => {
+export interface IAccordion {
+    name: string,
+    title: string,
+    description: string
+}
+
+interface IProps {
+    accordions: IAccordion[]
+}
+
+const AccordionCustom = ({accordions}: IProps) => {
     const [expanded, setExpanded] = React.useState<string | false>(false);
     const handleChange =
         (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
             setExpanded(isExpanded ? panel : false);
         };
-    return (
-        <Accordion expanded={expanded === name} onChange={handleChange(name)}>
-            {children}
-        </Accordion>
-    );
+
+    return <>
+        {
+            accordions.map(item => (
+                <Accordion key={item.name} expanded={expanded === item.name} onChange={handleChange(item.name)} itemScope itemType="https://schema.org/Question">
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls={`${item.name}-content`}
+                            id={`${item.name}-header`}
+                        >
+                            <Typography itemProp="name">{item.title}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography itemProp="text">
+                                {item.description}
+                            </Typography>
+                        </AccordionDetails>
+                </Accordion>
+            ))
+        }
+    </>
 };
 
 export default AccordionCustom;
